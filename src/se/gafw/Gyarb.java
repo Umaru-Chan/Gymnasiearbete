e5613fb12c5fa0f954f91e74e066c5c6f09da96e
Alexander <lolipoppen1@gmail.com>
2016-10-11 09:54:51 +0200

    Add files via upload

---
+++ src/se/gafw/Gyarb.java
@@ -0,0 +1,84 @@
+package se.gafw;
+
+import java.awt.Canvas;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
+import se.gafw.graphics.Screen;
+import se.gafw.graphics.Window;
+
+public class Gyarb extends Canvas{
+	
+	public static final int WIDTH = 400, HEIGHT = WIDTH / 16 * 9, SCALE = 4;
+	public static final short VERSION_MAJOR = 0, VERSION_MINOR = 1;
+	public static final String TITLE = "gyarb "+VERSION_MAJOR+"."+VERSION_MINOR;
+	
+	private boolean running;
+	private Window window;
+	private Screen screen;
+	
+	private BufferedImage image;
+	private int[] pixels;
+	
+	public Gyarb(){
+		window = new Window(TITLE , WIDTH * SCALE, HEIGHT * SCALE, this, true);
+		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+		pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+		running = true;
+		run();
+	}
+	
+	public void run(){
+		int frames = 0, updates = 0;
+		double delta = 0.0D, ns = 1e9 / 60.0D;
+		long lastTime = System.nanoTime();
+		long timer = System.currentTimeMillis();
+		
+		this.requestFocus();
+		while(running){
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while(delta >= 1){
+				update();
+				updates++;
+				delta--;
+			}
+			render();
+			frames++;
+			if(System.currentTimeMillis() - timer >= 1e3){
+				window.setTitle(TITLE+" fps: "+frames+" ups: "+updates);
+				updates = frames = 0;
+				timer += 1e3;
+			}
+		}
+	}
+	
+	private void render(){
+		BufferStrategy buffer = getBufferStrategy();
+		Graphics g = buffer.getDrawGraphics();
+		
+		//TODO rendera saker här
+		
+		
+		
+		//sluta rendera här
+		
+		System.arraycopy(screen.pixels, 0, pixels, 0, pixels.length);
+		g.drawImage(image, 0, 0, WIDTH * SCALE, HEIGHT * SCALE, null);
+		
+		g.dispose();
+		buffer.show();
+	}
+	
+	private void update(){
+		
+	}
+	
+	public static void main(String []args){
+		new Gyarb();
+	}
+
+}
