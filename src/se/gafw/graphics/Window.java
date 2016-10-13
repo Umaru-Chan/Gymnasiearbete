cd1c4e91c1f70e088265f30e543baa9f36298953
TheDahlman <1998dahlman@gmail.com>
2016-10-13 15:16:29 +0200

    Update Window.java

--- src/se/gafw/graphics/Window.java
+++ src/se/gafw/graphics/Window.java
@@ -10,6 +10,15 @@ public class Window {
     private JFrame frame;
     private Dimension dim;
     
+    /**
+     * Constructor. Creates a window
+     * 
+     * @param title The windows title
+     * @param width The windows width
+     * @param height The windows height
+     * @param canvas The game draws everything on the canvas 
+     * @param visible If the window is visible or not
+     */
     public Window(String title, int width, int height, Canvas canvas, boolean visible) {
         frame = new JFrame(title);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -22,14 +31,29 @@ public class Window {
         frame.setVisible(visible);
     }
     
+    /**
+     * Decides if the window is visible or not.
+     * 
+     * @param visible
+     */
     public void setVisible(boolean visible) {
         frame.setVisible(visible);
     }
     
+    /**
+     * Returns the dimension of the window
+     * 
+     * @return
+     */
     public Dimension getDimension() {
         return dim;
     }
     
+    /**
+     * Changes the title of the game
+     * 
+     * @param title 
+     */
     public void setTitle(String title) {
         frame.setTitle(title);
     }
