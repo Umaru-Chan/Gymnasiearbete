f242c4f302d1d7ebc5d08dec3cbd7334888c19bd
Alexander <lolipoppen1@gmail.com>
2016-10-21 12:39:38 +0200

    Update Tile.java

--- src/se/gafw/level/Tile.java
+++ src/se/gafw/level/Tile.java
@@ -7,13 +7,27 @@ public abstract class Tile {
 	
 	private final Sprite sprite;
 	
+	/**
+	 * @param sprite
+	 */
 	public Tile(Sprite sprite){ this.sprite = sprite; }
 	
+	/**
+	 * renders the tile at x, y
+	 * @param screen
+	 * @param x
+	 * @param y
+	 */
 	public void render(Screen screen, int x, int y)
 	{
 		screen.renderSprite(sprite, x, y, false);
 	}
-	
+	/**
+	 * @return true if the tile is solid, else return false.
+	 */
 	public boolean solid(){ return false; }
+	/**
+	 * @return true if the tile is liquid, else return false.
+	 */
 	public boolean liquid(){ return false; }
 }
