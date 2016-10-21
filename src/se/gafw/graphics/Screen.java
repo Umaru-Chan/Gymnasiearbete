252b5db00e5e47f8744eec8f2a0c805759a5e758
Alexander <lolipoppen1@gmail.com>
2016-10-21 12:38:11 +0200

    Update Screen.java

--- src/se/gafw/graphics/Screen.java
+++ src/se/gafw/graphics/Screen.java
@@ -5,6 +5,12 @@ public class Screen {
 	public int[] pixels;
 	private final int clearColor, width, height;
 	
+	/**
+	 * used to render to the screen
+	 * @param width
+	 * @param height
+	 * @param clearColor
+	 */
 	public Screen(int width, int height, int clearColor){
 		pixels = new int[width * height];
 		this.clearColor = clearColor;
@@ -12,19 +18,30 @@ public class Screen {
 		this.height = height;
 	}
 	
+	/**
+	 * clears all the pixels
+	 */
 	public void clear(){
 		for(int i = 0; i < pixels.length; i++)
 			pixels[i] = clearColor;
 	}
 	
+	/**
+	 * renders Sprite sprite at x, y.
+	 * clipped is not yet implemented.
+	 * 
+	 * @param sprite
+	 * @param x
+	 * @param y
+	 * @param clipped
+	 */
 	public void renderSprite(Sprite sprite, int x, int y, boolean clipped /*TODO*/){
 		for(int v = 0; v < sprite.height; v++){
 			int vp = v + y;
-			
-			if(vp < 0 || vp > height)continue;
+			if(vp < 0 || vp >= height)continue;
 			for(int u = 0; u < sprite.width; u++){
 				int up = u + x;
-				if(up < 0 || up > width)continue;
+				if(up < 0 || up >= width)continue;				
 				pixels[up + vp * width] = sprite.pixels[u + v * sprite.width];
 			}
 		}	
