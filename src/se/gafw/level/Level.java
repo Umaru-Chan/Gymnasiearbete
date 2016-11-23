edcfc8bc76a633db9b998f0e0cc644ac2eac9efd
Alexander <lolipoppen1@gmail.com>
2016-11-23 13:37:06 +0100

    Update Level.java

--- src/se/gafw/level/Level.java
+++ src/se/gafw/level/Level.java
@@ -1,30 +1,81 @@
 package se.gafw.level;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
 
 import se.gafw.gameObjects.Entity;
 import se.gafw.graphics.Screen;
 
 public class Level {
 
+	public static final Level TEST = new Level(256, 256);
+	
 	private int width, height;
-	private Tile[] tiles;
+	//private Tile[] tiles;
+	private int[] tileColors;
 	private ArrayList<Entity> entities = new ArrayList<>();
 	
+	public Level(String path){
+		try{
+			//ladda leveln...
+			BufferedImage image = ImageIO.read(getClass().getResourceAsStream(path));
+			this.width = image.getWidth();
+			this.height = image.getHeight();
+			image.getRGB(0,0,width, height, tileColors, 0, width);
+			//tiles = new Tile[tileColors.length];
+		}catch(IOException e){
+			System.err.println(" failed loading level!!");
+			e.printStackTrace();
+		}
+	}
+	
+	//använd inte atm
+	@Deprecated
 	public Level(int width, int height) {
 		this.width = width;
 		this.height = height;
-		tiles = new Tile[width * height];
+		//tiles = new Tile[width * height];
+		tileColors = new int[width * height];
 	}
 	
-	public void render(Screen screen, int x, int y) {
-		for (int i = 0; x < entities.size(); i++) {
+	public void render(Screen screen, int xOffs, int yOffs) {
+		int x0 = xOffs >> 4, x1 = x0 + 16;
+		int y0 = yOffs >> 4, y1 = y0 + 16;
+		//TODO matte
+		//if(xp < 0 || xp >= width)
+		
+		for(int y = y0; y < y1; y++){
+			for(int x = x0; x < x1; x++){
+				getTile(x, y).render(screen, x, y);
+			}
+		}
+		
+		//rendera allt över alla tiles
+		renderEntities(screen);
+	}
+	
+	private Tile getTile(int x, int y){
+		if(x < 0 || x >= width || y < 0 || y >= height)return Tile.VOID_TILE;
+		
+		switch(tileColors[x + y * width]){
+		case Tile.VOID_COLOR:return Tile.VOID_TILE;
+		}
+		
+		return Tile.VOID_TILE;
+	}
+	
+	private void renderEntities(Screen screen){
+		for (int i = 0; i < entities.size(); i++) {
 			entities.get(i).render(screen);
 		}
 	}
 	
 	public void update() {
-		
+		for (int i = 0; i < entities.size(); i++)
+			entities.get(i).update();
 	}
 	
 	public void addEntity(Entity entity) {
