3e3911ca116bc8fa06b058c76180c7fe341403a8
TheDahlman <1998dahlman@gmail.com>
2016-11-30 21:45:44 +0100

    Update Sprite.java

--- src/se/gafw/graphics/Sprite.java
+++ src/se/gafw/graphics/Sprite.java
@@ -4,7 +4,7 @@ public class Sprite {
 	
 	public static final Sprite VOID = new Sprite(SpriteSheet.TILES, 0, 0, 16);
 	public static final Sprite TEST = new Sprite(SpriteSheet.TILES, 1, 0, 16);
-	public static final Sprite PLAYER = new Sprite(SpriteSheet.TILES, 2, 2, 32);
+	public static final Sprite PLAYER = new Sprite(SpriteSheet.TILES, 0, 7, 32);
 	public static final Sprite INVENTORY = new Sprite(SpriteSheet.INVENTORY, 0, 0, 46, 16);
 	
 	public int[] pixels;
