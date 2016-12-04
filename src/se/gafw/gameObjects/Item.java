3b0045616b719437d543b985a8798ac4422dffa0
TheDahlman <1998dahlman@gmail.com>
2016-12-04 16:46:28 +0100

    Update Item.java

--- src/se/gafw/gameObjects/Item.java
+++ src/se/gafw/gameObjects/Item.java
@@ -6,8 +6,8 @@ import se.gafw.graphics.Sprite;
 public class Item {
 	
 	private Sprite sprite;
-	private final String name; 
-		
+	private final String name; 	
+	
 	public Item(Sprite sprite, String name) {
 		this.sprite = sprite;
 		this.name = name;
@@ -16,11 +16,7 @@ public class Item {
 	public void render(Screen screen, int x, int y) {
 		screen.renderSprite(sprite, x, y, false);
 	}
-	
-	public void update() {
 		
-	}
-	
 	public String getName() {
 		return name;
 	}
