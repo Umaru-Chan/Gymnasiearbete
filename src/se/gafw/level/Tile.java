69cfc1b3de48f9d4f0e90b1b620065ee08ef9f84
Alexander <lolipoppen1@gmail.com>
2017-01-16 23:38:14 +0100

    Update Tile.java

--- src/se/gafw/level/Tile.java
+++ src/se/gafw/level/Tile.java
@@ -2,14 +2,17 @@ package se.gafw.level;
 
 import se.gafw.graphics.Screen;
 import se.gafw.graphics.Sprite;
+import se.gafw.graphics.SpriteSheet;
 
 public abstract class Tile {
 	
 	public static final Tile LMAO = new AirTile(new Sprite(SpriteSheet.TILES, 4,4,16));//TEMP
 	public static final Tile VOID_TILE = new AirTile(Sprite.VOID);
 	public static final int VOID_COLOR = 0xffffffff;
-	public static final Tile TEST_TILE = new CollidableTile(Sprite.TEST);
-	public static final int TEST_COLOR = 0xff000000;
+	public static final Tile GRASS_TILE = new CollidableTile(Sprite.GRASS);
+	public static final int GRASS_COLOR = 0xff000000;
+	public static final Tile DIRT_TILE = new CollidableTile(Sprite.DIRT);
+	public static final int DIRT_COLOR = 0xaa88aa;
 	
 	
 	public final Sprite sprite;
