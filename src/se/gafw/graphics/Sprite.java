515fd1f4bc49d24a281f525bd745d61f5c729ee2
Alexander <lolipoppen1@gmail.com>
2017-02-21 08:53:35 +0100

    Update Sprite.java

--- src/se/gafw/graphics/Sprite.java
+++ src/se/gafw/graphics/Sprite.java
@@ -43,5 +43,14 @@ public class Sprite {
 			}
 		}
 	}
+	
+		public Sprite(int width, int height, int color)
+	{
+		this.width = width;
+		this.height = height;
+		pixels = new int[width * height];
+		
+		for(int i = 0; i < pixels.length; i++) pixels[i] = color;
+	}
 
 }
