93ae1a679654941f43d6185519938a0bb00af46a
Alexander <lolipoppen1@gmail.com>
2017-02-21 08:06:31 +0100

    Update Window.java

--- src/se/gafw/graphics/Window.java
+++ src/se/gafw/graphics/Window.java
@@ -6,32 +6,31 @@ import java.awt.event.WindowEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 import se.gafw.Gyarb;
+import se.gafw.gameState.InGame;
 import se.gafw.util.FileHandler;
 
 public class Window {
     
     private JFrame frame;
     private Dimension dim;
-    private Gyarb mainGame;
+    private JPanel panel;
     
     public Window(String title, int width, int height, Gyarb gyarb, boolean visible) {
-    	mainGame = gyarb;
         frame = new JFrame(title);
         frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+       
         frame.addWindowListener(new WindowAdapter(){
-        	@Override
         	public void windowClosing(WindowEvent e){
         		new Thread(() -> {        			
         			//TODO kollar varför det e så segt :(
         			int option = JOptionPane.showConfirmDialog(frame, "are you sure that you want to exit?", "confirmation", JOptionPane.YES_NO_OPTION);
         			if(option == JOptionPane.YES_OPTION)
                     {
-                        frame.dispose();
-                        frame = null;
+                        gyarb.stop();
                         System.gc();
-                        System.exit(0);
                     }
         			//om man vill stänga
         			option = JOptionPane.showConfirmDialog(frame, "Save?", "exiting", JOptionPane.YES_NO_OPTION);
@@ -43,20 +42,39 @@ public class Window {
         		}).start();
         	}
         });
+        
         frame.setResizable(false);
         frame.setPreferredSize(dim = new Dimension(width, height));
-        frame.setMaximumSize(dim = new Dimension(width, height));
-        frame.setMinimumSize(dim = new Dimension(width, height));
+        frame.setMaximumSize(dim);
+        frame.setMinimumSize(dim);
         frame.setLocationRelativeTo(null);
-        frame.add(gyarb);
+        frame.setFocusable(true);
+        //frame.setAutoRequestFocus(true);
+        
+        //Image image;
+        //TODO load image
+        //frame.setIconImage(image);
+        frame.add((panel = new JPanel()).add(gyarb));
+        
+        panel.setFocusable(true);
+        panel.requestFocus();
+        
         frame.setVisible(visible);
-        gyarb.start();
     }
     
+    public JPanel getPanel()
+    {
+    	return panel;
+    }
+    
+    public JFrame getFrame()
+    {
+    	return frame;
+    }
     
     private void save(){
     	FileHandler handler = new FileHandler("res/saves/", "save", "ser");
-    	handler.writeObject(mainGame.getCurrentLevel());
+    	handler.writeObject(InGame.getCurrentLevel());
     }
     
     public void setVisible(boolean visible) {
