b1dd01c83c1fea2da0d84280eb167c508073a60b
Alexander <lolipoppen1@gmail.com>
2017-03-17 21:09:34 +0100

    Update CollidableTile.java

--- src/se/gafw/level/CollidableTile.java
+++ src/se/gafw/level/CollidableTile.java
@@ -3,18 +3,16 @@ package se.gafw.level;
 import se.gafw.graphics.Sprite;
 
 /**
- * 
- * Used when creating Tile objects that will return true when calling solid()
  *
+ * Used when creating Tile objects that will return false when calling solid()
+ * 
  */
-public class CollidableTile extends Tile{
+public class AirTile extends Tile {
 
-	public CollidableTile(Sprite sprite) {
-		super(sprite);
-	}
-	
-	public boolean solid(){
-		return true;
-	}
+    public AirTile(Sprite sprite){super(sprite);}
 
+    public boolean solid()
+    {
+        return false;
+    }
 }
