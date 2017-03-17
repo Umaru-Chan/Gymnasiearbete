1922859abf3ba6bc6739da546996c394eb519549
TheDahlman <1998dahlman@gmail.com>
2017-03-17 12:46:05 +0100

    Update Item.java

--- src/se/gafw/gameObjects/Item.java
+++ src/se/gafw/gameObjects/Item.java
@@ -3,21 +3,24 @@ package se.gafw.gameObjects;
 import se.gafw.graphics.Screen;
 import se.gafw.graphics.Sprite;
 
+/**
+ * Item class represents items that the player can pick up and store in a Inventory.
+ */
 public class Item {
 	
-	private Sprite sprite;
-	private String name;
-	int x, y;
+	private Sprite sprite; //The sprite for the item.
+	private String name; //The name of the item.
+	private int x, y;
 	
-	public static final Item DIRT = new Item(Sprite.DIRT, "Dirt");
-	public static final Item GRASS = new Item(Sprite.GRASS, "Grass");
-	public static final Item STONE = new Item(Sprite.STONE, "Stone");
-	public static final Item RUBY = new Item(Sprite.RUBY, "Ruby");
+	public static final Item DIRT = new Item(Sprite.DIRT, "Dirt"); 		//Dirt item
+	public static final Item GRASS = new Item(Sprite.GRASS, "Grass");	//Grass item
+	public static final Item STONE = new Item(Sprite.STONE, "Stone"); 	//Stone item
+	public static final Item DIAMOND = new Item(Sprite.DIAMOND, "Ruby");//Diamond item
 	
 	/**
-	 * 
-	 * @param sprite
-	 * @param name
+	 * Class constructor. Creates a new item with a sprite and a name.
+	 * @param sprite is the sprite for the item
+	 * @param name is the name of the item
 	 */
 	public Item(Sprite sprite, String name) {
 		this.sprite = sprite;
@@ -25,10 +28,10 @@ public class Item {
 	}
 	
 	/**
-	 * 
-	 * @param screen
-	 * @param x
-	 * @param y
+	 * Renders the items on a specified x and y coordinate.
+	 * @param screen for rendering the items.
+	 * @param x is the x coordinate for the item.
+	 * @param y is the y coordinate for the item.
 	 */
 	public void render(Screen screen, int x, int y) {
 		this.x = x;
@@ -37,32 +40,32 @@ public class Item {
 	}
 		
 	/**
-	 * 
-	 * @return
+	 * Return the name of the item.
+	 * @return name, the name of the item.
 	 */
 	public String getName() {
 		return name;
 	}
 	
 	/**
-	 * 
-	 * @return
+	 * Return the sprite of the item.
+	 * @return sprite, the sprite the item has.
 	 */
 	public Sprite getSprite() {
 		return sprite;
 	}
 	
 	/**
-	 * 
-	 * @return
+	 * Return horizontal x-coordinate for the item.
+	 * @return x, x-coordinate for the item
 	 */
 	public int getX() {
 		return x;
 	}
 	
 	/**
-	 * 
-	 * @return
+	 * Return vertical y-coordinate for the item.
+	 * @return y, y-coordinate for the item.
 	 */
 	public int getY() {
 		return y;
