5a355892fb50673dcfa07b405cf0981b0b69c411
TheDahlman <1998dahlman@gmail.com>
2017-03-17 12:47:12 +0100

    Update NPC.java

--- src/se/gafw/gameObjects/NPC.java
+++ src/se/gafw/gameObjects/NPC.java
@@ -4,28 +4,43 @@ import se.gafw.graphics.Screen;
 import se.gafw.graphics.Sprite;
 import se.gafw.level.Level;
 
+/**
+ * The NPC class is the no player creatures, the creatures that the player do
+ * not have control over. 
+ */
 public class NPC extends Entity {
 
 	@SuppressWarnings("unused")
-	private float health, speed;
+	private float health, speed; //Health and speed for the creature.
 	
+	/**
+	 * Class constructor. Creates a NPC.
+	 * @param level
+	 * @param x, the x-coordinate for the NPC.
+	 * @param y, the y-coordinate for the NPC.
+	 * @param sprite, the sprite for the NPC.
+	 * @param health, how much health the NPC has.
+	 * @param speed, how fast the NPC move.
+	 */
 	public NPC(Level level, float x, float y, Sprite sprite, float health, float speed) {
-		super(level, x, y, sprite);
+		super(level, x, y, sprite); //Calls the super constructor
 		this.health = health;
 		this.speed = speed;
 	}
 	
+	/**
+	 * 
+	 */
 	public void update() {
-		//TODO röra på skiten??
-	}
-	
-	//TODO metod för att attakera spelaren / andra nps:s
-	
-	//TODO metod för ngn special-sak, vad vet jag??
+		
+	}	
 
-	
+	/**
+	 * Renders the NPC on the window.
+	 * @param screen 
+	 */
 	public void render(Screen screen) {
-		//TODO animationer??
+		//TODO animations
 		screen.renderSprite(sprite, (int)x, (int)y, false);
 	}
 }
